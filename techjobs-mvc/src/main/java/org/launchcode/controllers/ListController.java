2b05dfb33b47cd300053bb1e014dad3864f24d81
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-07-03 17:47:10 -0500

    Add files via upload

---
+++ techjobs-mvc/src/main/java/org/launchcode/controllers/ListController.java
@@ -0,0 +1,62 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping(value = "list")
+public class ListController {
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public ListController () {
+        columnChoices.put("core competency", "Skill");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("position type", "Position Type");
+        columnChoices.put("all", "All");
+    }
+
+    @RequestMapping(value = "")
+    public String list(Model model) {
+
+        model.addAttribute("columns", columnChoices);
+
+        return "list";
+    }
+
+    @RequestMapping(value = "values")
+    public String listColumnValues(Model model, @RequestParam String column) {
+
+        if (column.equals("all")) {
+            ArrayList<HashMap<String, String>> results = JobData.findAll(); //changed jobs to results
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("results", results); //changed from "jobs", jobs to "results", results
+            return "list-jobs";
+        } else {
+            ArrayList<String> items = JobData.findAll(column);
+            model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
+            model.addAttribute("column", column);
+            model.addAttribute("items", items);
+            return "list-column";
+        }
+
+    }
+
+    @RequestMapping(value = "jobs")
+    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) {
+        ArrayList<HashMap<String, String>> results = JobData.findByColumnAndValue(column, value);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        model.addAttribute("results", results); //changed "jobs", jobs to "results", results
+        return "list-jobs";
+    }
+}
